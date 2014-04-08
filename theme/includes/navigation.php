63382e4a58df7557be90c578331e9eef281e6ce7
Potiguar Catalan <potiguar@potiguar.net>
2014-04-08 02:22:38 -0300

    Register main menu location

--- theme/includes/navigation.php
+++ theme/includes/navigation.php
@@ -1,4 +1,8 @@
 <?php
 
+function inmetrics_register_top_menu() {
+  register_nav_menu('inmetrics_main_menu_location', __('Top Menu', 'inmetrics'));
+}
+add_action('init', 'inmetrics_register_top_menu');
 
 ?>
