a05a3ba0bd31cbcd7924bc78df2cfddc53c3dd23
Potiguar Catalan <potiguar@potiguar.net>
2014-04-08 02:26:32 -0300

    Add localization files and load theme text domain

--- theme/includes/localization.php
+++ theme/includes/localization.php
@@ -3,4 +3,10 @@
 // Hide WPML's "professional translation" boxes
 define('ICL_DONT_PROMOTE', TRUE);
 
+function inmetrics_load_theme_textdomain() {
+  $lang_dir = get_template_directory() . '/lang';
+  load_theme_textdomain('inmetrics', $lang_dir);
+}
+add_action('after_setup_theme', 'inmetrics_load_theme_textdomain');
+
 ?>
