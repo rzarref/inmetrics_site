ae8498fc86bcc9bc7f2db09596cb953a2255bf28
Potiguar Catalan <potiguar@potiguar.net>
2014-04-10 14:51:33 -0300

    Improve fix for symlinks

--- theme/includes/custom_fields.php
+++ theme/includes/custom_fields.php
@@ -1,24 +1,26 @@
 <?php
 
+define('ACF_LITE' , true);
+
 // Fix for symlinks
 function inmetrics_fix_acf_dir($file) {
   $base = get_template_directory();
-  if(is_link($base)) {
-    $base = readlink($base);
-    $dir = get_template_directory_uri();
-    $dir = str_replace($base, $dir, $file);
-    return str_replace('\\' ,'/', $dir);
-  }
-  else {
-    return acf.helpers_get_dir($file);
-  }
+  $base = readlink($base);
+  $dir = get_template_directory_uri();
+  $dir = str_replace($base, $dir, $file);
+  return str_replace('\\' ,'/', $dir);
 }
 
 if(!class_exists('Acf')) {
-  define('ACF_LITE' , true);
-  add_filter('acf/helpers/get_dir', 'inmetrics_fix_acf_dir', 1, 1);
-  require_once(trailingslashit(get_template_directory()) . "vendor/advanced-custom-fields/acf.php");
-  remove_filter('acf/helpers/get_dir', array(acf, 'helpers_get_dir'), 1, 1);
+  $base = get_template_directory();
+  if(is_link($base)) {
+    add_filter('acf/helpers/get_dir', 'inmetrics_fix_acf_dir', 1, 1);
+    require_once(trailingslashit(get_template_directory()) . "vendor/advanced-custom-fields/acf.php");
+    remove_filter('acf/helpers/get_dir', array(acf, 'helpers_get_dir'), 1, 1);
+  }
+  else {
+    require_once(trailingslashit(get_template_directory()) . "vendor/advanced-custom-fields/acf.php");
+  }
 }
 
 function inmetrics_acf_options_hide_all() {
