d902a69f6b2fa0976bd9ff9fcdcc24e9d4e910ec
Potiguar Catalan <potiguar@potiguar.net>
2014-04-10 10:58:58 -0300

    fixup! Add efficiency tables

--- theme/includes/helpers.php
+++ theme/includes/helpers.php
@@ -4,16 +4,4 @@ function t_url($path) {
   echo(trailingslashit(get_template_directory_uri()) . $path);
 }
 
-function t_img($field) {
-  $image = get_field($field);
-  if(is_array($image)) {
-    $title = $image['title'];
-    $alt = $image['alt'];
-    $url = $image['url'];
-    $width = $image['width'];
-    $height = $image['height'];
-    echo('<img src="'. $url . '" alt="' . $alt .'" title="' . $title . '" width="' . $width . '" height="' . $height . '" />');
-  }
-}
-
 ?>
