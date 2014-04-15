b28134e9f451a971830305811fb1a4b5e6399236
Potiguar Catalan <potiguar@potiguar.net>
2014-04-15 12:19:11 -0300

    Add direct link to js files

--- theme/footer.php
+++ theme/footer.php
@@ -1,4 +1,6 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="<?php t_url("js/inmetrics/animations.js"); ?>"></script>
+    <script src="<?php t_url("js/inmetrics/init.js"); ?>"></script>
     <script src="<?php t_url("js/inmetrics.min.js"); ?>"></script>
     <?php wp_footer(); ?>
   </body>
