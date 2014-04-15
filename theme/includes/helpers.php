bbd9875954ccb8c6c51d393f61d509d943fbbc78
Potiguar Catalan <potiguar@potiguar.net>
2014-04-15 20:05:57 -0300

    Add helpers that don't echo

--- theme/includes/helpers.php
+++ theme/includes/helpers.php
@@ -1,10 +1,18 @@
 <?php
 
 function t_url($path) {
-  echo(trailingslashit(get_template_directory_uri()) . $path);
+  echo(rel_url($path));
 }
 
-function t_img($field, $post_id = null) {  
+function rel_url($path) {
+  return trailingslashit(get_template_directory_uri()) . $path;
+}
+
+function t_img($field, $post_id = null) {
+  echo img_tag($field, $post_id);
+}
+
+function img_tag($field, $post_id = null) {
   $image = get_field($field, $post_id);
   if(is_array($image)) {
     $title = $image['title'];
@@ -12,7 +20,7 @@ function t_img($field, $post_id = null) {
     $url = $image['url'];
     $width = $image['width'];
     $height = $image['height'];
-    echo('<img src="'. $url . '" alt="' . $alt .'" title="' . $title . '" width="' . $width . '" height="' . $height . '" />');
+    return '<img src="'. $url . '" alt="' . $alt .'" title="' . $title . '" width="' . $width . '" height="' . $height . '" />';
   }
 }
 
