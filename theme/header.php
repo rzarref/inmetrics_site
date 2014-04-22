f4a992f7dd8fd268770b20c4e16ee97eb3468bd6
Potiguar Catalan <potiguar@potiguar.net>
2014-04-22 20:48:18 +0100

    Fix viewport scale (Fixes #6)

--- theme/header.php
+++ theme/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title><?php wp_title( '|', true, 'right' ); ?></title>    
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link href='//fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
